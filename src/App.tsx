750c7d07ed7d6f69b1a96a1ad80c4d0e5902b7d5
Garvin <gtan36@student.monash.edu>
2021-03-05 15:02:49 +1100

    initialise main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import Header from "./components/Header";
+import MainPage from "./components/MainPage";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MainPage />
     </div>
   );
 }
