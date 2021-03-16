a3a5dc1aad27a0e4bad7a41ede0fe0cd16b46826
minh-tn <tngu0016@student.monash.edu>
2021-03-17 09:36:06 +1100

    Add a simple app bar as header

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import logo from './logo.svg';
 import Header from "./components/Header";
 import MainPage from "./components/MainPage";
-import './App.css';
 
 function App() {
   return (
