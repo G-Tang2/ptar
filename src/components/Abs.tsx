e602fa4bd7d3a0b4f1758e142e545df5077e6e4e
Garvin <gtan36@student.monash.edu>
2021-03-17 14:15:29 +1100

    Converted ABS class component to functional component

--- src/components/Abs.tsx
+++ src/components/Abs.tsx
@@ -1,152 +1,144 @@
-import React from "react";
+import React, {useState} from "react";
 import * as Survey from "survey-react";
 import "./Abs.css"
 import "survey-react/survey.css";
 
-class FormComponent extends React.Component<any, any> {
-    constructor(props: any) {
-        super(props);
-        this.state = { isCompleted: false };
-        this.onCompleteComponent = this.onCompleteComponent.bind(this);
-    }
-    onCompleteComponent() {
-        this.setState({ isCompleted: true });
-    }
-    render() {
-        let questionJson = {
-            questions: [
-                {
-                    type: "radiogroup",
-                    name: "q1",
-                    title: "Short attention span, easy distractibility, inability to concentreate.",
-                    isRequired: true,
-                    colCount: 4,
-                    choices: ["1", "2", "3", "4"]
-                },
-                {
-                    type: "radiogroup",
-                    name: "q2",
-                    title: "Impulsive, impatient, low tolerance for pain or frustration.",
-                    isRequired: true,
-                    colCount: 4,
-                    choices: ["1", "2", "3", "4"]
-                },
-                {
-                    type: "radiogroup",
-                    name: "q3",
-                    title: "Uncooperative, resistant to care, demanding.",
-                    isRequired: true,
-                    colCount: 4,
-                    choices: ["1", "2", "3", "4"]
-                },
-                {
-                    type: "radiogroup",
-                    name: "q4",
-                    title: "Violent and/or threatening violence toward people or property.",
-                    isRequired: true,
-                    colCount: 4,
-                    choices: ["1", "2", "3", "4"]
-                },
-                {
-                    type: "radiogroup",
-                    name: "q5",
-                    title: "Explosive and/or unpredictable anger.",
-                    isRequired: true,
-                    colCount: 4,
-                    choices: ["1", "2", "3", "4"]
-                },
-                {
-                    type: "radiogroup",
-                    name: "q6",
-                    title: "Rocking, rubbing, moaning, or other self-stimulating behaviours.",
-                    isRequired: true,
-                    colCount: 4,
-                    choices: ["1", "2", "3", "4"]
-                },
-                {
-                    type: "radiogroup",
-                    name: "q7",
-                    title: "Pulling at tubes, restraints etc.",
-                    isRequired: true,
-                    colCount: 4,
-                    choices: ["1", "2", "3", "4"]
-                },
-                {
-                    type: "radiogroup",
-                    name: "q8",
-                    title: "Wandering from treatment areas.",
-                    isRequired: true,
-                    colCount: 4,
-                    choices: ["1", "2", "3", "4"]
-                },
-                {
-                    type: "radiogroup",
-                    name: "q9",
-                    title: "Restlessness, pacing, excessive movement.",
-                    isRequired: true,
-                    colCount: 4,
-                    choices: ["1", "2", "3", "4"]
-                },
-                {
-                    type: "radiogroup",
-                    name: "q10",
-                    title: "Repetitive behaviours, motor and/or verbal.",
-                    isRequired: true,
-                    colCount: 4,
-                    choices: ["1", "2", "3", "4"]
-                },
-                {
-                    type: "radiogroup",
-                    name: "q11",
-                    title: "Rapid, loud or excessive talking.",
-                    isRequired: true,
-                    colCount: 4,
-                    choices: ["1", "2", "3", "4"]
-                },
-                {
-                    type: "radiogroup",
-                    name: "q12",
-                    title: "Sudden changes in mood.",
-                    isRequired: true,
-                    colCount: 4,
-                    choices: ["1", "2", "3", "4"]
-                },
-                {
-                    type: "radiogroup",
-                    name: "q13",
-                    title: "Easily initiated or excessive crying and/or laughter.",
-                    isRequired: true,
-                    colCount: 4,
-                    choices: ["1", "2", "3", "4"]
-                },
-                {
-                    type: "radiogroup",
-                    name: "q14",
-                    title: "Self-abusiveness, physical and/or verbal.",
-                    isRequired: true,
-                    colCount: 4,
-                    choices: ["1", "2", "3", "4"]
-                }
-            ]
-        };
-        var surveyRender = !this.state.isCompleted ? (
-            <Survey.Survey
-            json={questionJson}
-            showCompletedPage={false}
-            onComplete={this.onCompleteComponent}
-            />
-        ) : null;
-        var onCompleteComponent = this.state.isCompleted ? (
-            <div>You have successfully complete the ABS form!</div>
-        ) : null;
+function FormComponent() {
+    const [isCompleted, setIsCompleted] = useState(false);
 
-        return (
-            <div>
-                {surveyRender}
-                {onCompleteComponent}
-            </div>
-        );
-    }
+    const questionJson = {
+        questions: [
+            {
+                type: "radiogroup",
+                name: "q1",
+                title: "Short attention span, easy distractibility, inability to concentreate.",
+                isRequired: true,
+                colCount: 4,
+                choices: ["1", "2", "3", "4"]
+            },
+            {
+                type: "radiogroup",
+                name: "q2",
+                title: "Impulsive, impatient, low tolerance for pain or frustration.",
+                isRequired: true,
+                colCount: 4,
+                choices: ["1", "2", "3", "4"]
+            },
+            {
+                type: "radiogroup",
+                name: "q3",
+                title: "Uncooperative, resistant to care, demanding.",
+                isRequired: true,
+                colCount: 4,
+                choices: ["1", "2", "3", "4"]
+            },
+            {
+                type: "radiogroup",
+                name: "q4",
+                title: "Violent and/or threatening violence toward people or property.",
+                isRequired: true,
+                colCount: 4,
+                choices: ["1", "2", "3", "4"]
+            },
+            {
+                type: "radiogroup",
+                name: "q5",
+                title: "Explosive and/or unpredictable anger.",
+                isRequired: true,
+                colCount: 4,
+                choices: ["1", "2", "3", "4"]
+            },
+            {
+                type: "radiogroup",
+                name: "q6",
+                title: "Rocking, rubbing, moaning, or other self-stimulating behaviours.",
+                isRequired: true,
+                colCount: 4,
+                choices: ["1", "2", "3", "4"]
+            },
+            {
+                type: "radiogroup",
+                name: "q7",
+                title: "Pulling at tubes, restraints etc.",
+                isRequired: true,
+                colCount: 4,
+                choices: ["1", "2", "3", "4"]
+            },
+            {
+                type: "radiogroup",
+                name: "q8",
+                title: "Wandering from treatment areas.",
+                isRequired: true,
+                colCount: 4,
+                choices: ["1", "2", "3", "4"]
+            },
+            {
+                type: "radiogroup",
+                name: "q9",
+                title: "Restlessness, pacing, excessive movement.",
+                isRequired: true,
+                colCount: 4,
+                choices: ["1", "2", "3", "4"]
+            },
+            {
+                type: "radiogroup",
+                name: "q10",
+                title: "Repetitive behaviours, motor and/or verbal.",
+                isRequired: true,
+                colCount: 4,
+                choices: ["1", "2", "3", "4"]
+            },
+            {
+                type: "radiogroup",
+                name: "q11",
+                title: "Rapid, loud or excessive talking.",
+                isRequired: true,
+                colCount: 4,
+                choices: ["1", "2", "3", "4"]
+            },
+            {
+                type: "radiogroup",
+                name: "q12",
+                title: "Sudden changes in mood.",
+                isRequired: true,
+                colCount: 4,
+                choices: ["1", "2", "3", "4"]
+            },
+            {
+                type: "radiogroup",
+                name: "q13",
+                title: "Easily initiated or excessive crying and/or laughter.",
+                isRequired: true,
+                colCount: 4,
+                choices: ["1", "2", "3", "4"]
+            },
+            {
+                type: "radiogroup",
+                name: "q14",
+                title: "Self-abusiveness, physical and/or verbal.",
+                isRequired: true,
+                colCount: 4,
+                choices: ["1", "2", "3", "4"]
+            }
+        ]
+    };
+    const surveyRender = !isCompleted ? (
+        <Survey.Survey
+        json={questionJson}
+        showCompletedPage={false}
+        onComplete={() => setIsCompleted(true)}
+        />
+    ) : null;
+    const onCompleteComponent = isCompleted ? (
+        <div>You have successfully complete the ABS form!</div>
+    ) : null;
+
+    return (
+        <div>
+            {surveyRender}
+            {onCompleteComponent}
+        </div>
+    );
 }
 
 function Abs() {
