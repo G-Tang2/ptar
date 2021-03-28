69d08b26d8f31caaebb39b02dd5900c1d45282bc
minh-tn <tngu0016@student.monash.edu>
2021-03-28 22:44:13 +1100

    Fix typo in ScoreBar

--- src/components/ScoreBar.tsx
+++ src/components/ScoreBar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function ScoreBar(props:any) {
     return (
     <div>
-        <span>Total Score</span>
+        <span>Total Score </span>
         <span>{props.score()}</span>
     </div>
     )
