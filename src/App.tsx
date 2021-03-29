5a15913456562f0aaad05bafe78d06a8a9182415
jyan0006@student.monash.edu <jyan0006@student.monash.edu>
2021-03-29 16:42:03 +0800

    added WptasPicture branch

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import Header from "./components/Header";
 import MainPage from "./components/MainPage";
+import WptasPicture from "./components/WptasPicture";
 import './stylesheets/main.scss';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Header />
       <MainPage />
+      <WptasPicture />
     </div>
   );
 }
---
+++ src/components/WptasPicture.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
+function PicCarousel() {
+    return (
+        <Carousel>
+            <div className = "PicCarousel">
+                <img src="./assets/bird1.jpg" />
+                <p className="pic1">A BIRD</p>
+            </div>
+            <div>
+                <img src="./assets/bird2.png" />
+                <p className="pic2">DEMO 2</p>
+            </div>
+            <div>
+                <img src="./assets/bird3.jpg" />
+                <p className="pic3">DEMO 3</p>
+            </div>
+        </Carousel>
+    );
+}
+
+function WptasPicture() {
+    return (
+        <div className = "WptasPicture">
+            <h1>WESTMEAD P.T.A SCALE - PICTURES</h1>
+            <p>Which pictures were shown yesterday?</p>
+            <PicCarousel />
+        </div>
+    );
+}
+
+export default WptasPicture;
