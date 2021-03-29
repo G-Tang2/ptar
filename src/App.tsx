3fa81ed38e54c5c2b1735dd2fa4a66edd1a73259
Garvin <gtan36@student.monash.edu>
2021-03-29 19:53:58 +1100

    Add qr instruction and start button

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import Header from "./components/Header";
 import MainPage from "./components/MainPage";
+import Qr from "./components/Qr";
 import './stylesheets/main.scss';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MainPage />
+      <Qr />
     </div>
   );
 }
