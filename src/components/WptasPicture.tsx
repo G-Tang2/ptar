342fd9c8da63dafbbecb536f602310adcc91bb26
jyan0006@student.monash.edu <jyan0006@student.monash.edu>
2021-04-23 18:51:29 +0800

    added state hook to record the number of images clicked

--- src/components/WptasPicture.tsx
+++ src/components/WptasPicture.tsx
@@ -12,28 +12,37 @@ import pic8 from './assets/bird8.jpg';
 import pic9 from './assets/bird9.jpg';
 
 function ImageSelector() {
-    const [isSelected, SetIsSelected] = useState(false);
+    const [count, setCount] = useState(0);
 
     const highlightEffect = {
-        'padding': '3px',
-        'backgroundColor': '#808080',
+        'padding': '5px',
+        'backgroundColor': '#00BFFF',
         'borderWidth': 'medium',
-        'borderStyle': 'dashed',
-        'borderColor': '#0000ff'
+        'borderStyle': 'solid',
+        'borderColor': '#00BFFF'
     };
 
     const highlightImage = () => {
+        if (count === 3) {
+            console.log('Cannot select more than 3 images.')
+            return;
+        }
+
         var images = document.getElementsByTagName('img');
         var imgCount = images.length;
         
         for (let i = 0; i < imgCount; i++) {
             images[i].onclick = function() {
                 if (images[i].style.borderStyle === highlightEffect.borderStyle) {
+                    setCount(count - 1);
+                    console.log(count);
                     images[i].style.padding = 'auto';
                     images[i].style.background = 'none';
                     images[i].style.border = 'none';
                 }
                 else {
+                    setCount(count + 1);
+                    console.log(count);
                     images[i].style.padding = highlightEffect.padding;
                     images[i].style.backgroundColor = highlightEffect.backgroundColor;
                     images[i].style.borderWidth = highlightEffect.borderWidth;
@@ -48,18 +57,18 @@ function ImageSelector() {
         <div className = "picsRow1">
             <div className = "pic1">
                 <img src={pic1} alt = "bird1" height = {200} width = {200} onClick={highlightImage}/>
-                <img src={pic2} alt = "bird2" height = {200} width = {200}/>
-                <img src={pic3} alt = "bird3" height = {200} width = {200}/>
+                <img src={pic2} alt = "bird2" height = {200} width = {200} onClick={highlightImage}/>
+                <img src={pic3} alt = "bird3" height = {200} width = {200} onClick={highlightImage}/>
             </div>
             <div className = "picsRow2">
-                <img src={pic4} alt = "bird4" height = {200} width = {200}/>
-                <img src={pic5} alt = "bird5" height = {200} width = {200}/>
-                <img src={pic6} alt = "bird6" height = {200} width = {200}/>
+                <img src={pic4} alt = "bird4" height = {200} width = {200} onClick={highlightImage}/>
+                <img src={pic5} alt = "bird5" height = {200} width = {200} onClick={highlightImage}/>
+                <img src={pic6} alt = "bird6" height = {200} width = {200} onClick={highlightImage}/>
             </div>
             <div className = "picRow3">
-                <img src={pic7} alt = "bird7" height = {200} width = {200}/>
-                <img src={pic8} alt = "bird8" height = {200} width = {200}/>
-                <img src={pic9} alt = "bird9" height = {200} width = {200}/>
+                <img src={pic7} alt = "bird7" height = {200} width = {200} onClick={highlightImage}/>
+                <img src={pic8} alt = "bird8" height = {200} width = {200} onClick={highlightImage}/>
+                <img src={pic9} alt = "bird9" height = {200} width = {200} onClick={highlightImage}/>
             </div>
         </div>
     )
