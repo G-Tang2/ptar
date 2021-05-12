1562b2a7fc288e57fdd400d0abf43ddbccc46d4a
Garvin <gtan36@student.monash.edu>
2021-05-12 11:27:18 +1000

    Scrolls to top of the page after QR login

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -3,8 +3,13 @@ import AvailableTest from './AvailableTest';
 import Progress from "./Progress";
 import PastTest from "./PastTest";
 import { Paper } from "@material-ui/core";
+import { useEffect } from "react";
 
 function MainPage(props) {
+
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    })
     return (<div className='main-container'>
         <Paper className="main-page-section" variant="outlined">
             <AvailableTest patientId={props.patientId}/>
