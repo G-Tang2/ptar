1efea58efa1f707f8fc4010184e8b6b3dacf37ac
Garvin <gtan36@student.monash.edu>
2021-05-21 16:18:20 +1000

    Add missing file for viewing past wptas test

---
+++ src/components/ViewPastWptasTest.tsx
@@ -0,0 +1,81 @@
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+
+function ViewPastWptasTest(props) {
+    const { testId } = props.match.params
+    const [questions, setQuestions] = useState<{wptas_question_no:number, wptas_question_desc:string}[]>([])
+    const [results, setResults] = useState<{wptas_mc_given:boolean, wptas_correct:boolean, wptas_ans_note:string}[]>([])
+    const [completeFetch, setCompleteFetch] = useState(false)
+
+    console.log(testId);
+    // get all patient's past test results
+    const getPastWptasTestResults = async () => {
+        fetch(`http://localhost:5000/view/wptas/${testId}`)
+        .then(res => res.json())
+        .then(res => setResults(res))
+        .then(res=>setCompleteFetch(true))
+    }
+
+    const getQuestions = () => {
+        fetch("http://localhost:5000/questions/wptas")
+        .then(res => res.json())
+        .then(res => setQuestions(res))
+    }
+
+    useEffect(() => {
+        getQuestions()
+        getPastWptasTestResults()
+    }, []);
+
+    const questionResults = (wptas_question_no:number, wptas_question_desc:string) => {
+        console.log(results)
+        const renderInfo = completeFetch ?
+        (
+            <TableRow key={wptas_question_no} >
+                <TableCell component="th" scope="row">
+                    {wptas_question_no + ". " + wptas_question_desc}
+                </TableCell>
+                <TableCell align="center">
+                    {results[wptas_question_no - 1].wptas_correct ? "Y" : "N"}
+                </TableCell>
+                <TableCell align="center">
+                    {results[wptas_question_no - 1].wptas_mc_given ? "Y" : "N"}
+                </TableCell>
+                <TableCell >
+                    {results[wptas_question_no - 1].wptas_ans_note}
+                </TableCell>
+            </TableRow>
+        )
+        : 
+        null
+        
+        return renderInfo
+    }
+
+    return (
+        <div className="main-container">
+            <h1>WPTAS Past Tests</h1>
+            <div className="questions">
+                <TableContainer component={Paper}>
+                    <Table>
+                        <TableHead >
+                            <TableRow>
+                                <TableCell>Question</TableCell>
+                                <TableCell align="center">Correct</TableCell>
+                                <TableCell align="center">MC Given</TableCell>
+                                <TableCell>Note</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                        {questions.map((question:{wptas_question_no:number, wptas_question_desc:string}) => (
+                            questionResults(question.wptas_question_no, question.wptas_question_desc)
+                        ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </div>
+        </div>
+    )
+}
+
+export default ViewPastWptasTest;
